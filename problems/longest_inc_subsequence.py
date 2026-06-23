b07ff4eaef1bd336b9b1914df1ca62a24987a8c9
manoje8 <manodeepan2001@gmail.com>
2026-06-24 00:19:44 +0530

    update lis

--- problems/longest_inc_subsequence.py
+++ problems/longest_inc_subsequence.py
@@ -18,8 +18,13 @@ To answer that, you must look back at all previous elements nums[j] where j < i.
 then you can take the best subsequence ending at j and simply append nums[i] to it.
 """
 
+import bisect
+
 
 def length_of_lis(arr):
+    """
+    Time: O(n²), Space: O(n). This works for n ≤ 2500.
+    """
     n = len(arr)
     dp = [1] * n
 
@@ -31,6 +36,38 @@ def length_of_lis(arr):
     return max(dp) if dp else 0
 
 
+def longest_increase_subsequence(arr):
+    """
+    Patience Sorting (O(n log n))	O(n log n)	O(n)
+    """
+    tail = []
+    for num in arr:
+        pos = bisect.bisect_left(tail, num)
+        if pos == len(tail):
+            tail.append(num)
+        else:
+            tail[pos] = num
+    return len(tail)
+
+
+"""
+[10,9,2,5,3,7,101,18]
+
+x=10: tails = [10]
+x=9: bisect_left([10], 9) = 0 → replace → tails = [9]
+x=2: bisect_left([9], 2) = 0 → replace → tails = [2]
+x=5: bisect_left([2], 5) = 1 → append → tails = [2,5]
+x=3: bisect_left([2,5], 3) = 1 → replace → tails = [2,3]
+(Note: We replaced 5 with 3. We lost the subsequence [2,5] but gained [2,3] which is better for future numbers like 4, 7, etc.)
+x=7: bisect_left([2,3], 7) = 2 → append → tails = [2,3,7]
+x=101: bisect_left([2,3,7], 101) = 3 → append → tails = [2,3,7,101]
+x=18: bisect_left([2,3,7,101], 18) = 3 → replace → tails = [2,3,7,18]
+"""
+
+
 if __name__ == "__main__":
     arr = [10, 9, 2, 5, 3, 7, 101, 18]
-    length_of_lis(arr)
+    res_1 = length_of_lis(arr)
+    res_2 = longest_increase_subsequence(arr)
+
+    print(res_1, res_2)
