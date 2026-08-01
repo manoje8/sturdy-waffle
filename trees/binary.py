898da875efe7ba8ec33fc6e645947c87e5ab4731
manoje8 <manodeepan2001@gmail.com>
2026-08-01 18:08:13 +0530

    added delete & isBST binary tree method

--- trees/binary.py
+++ trees/binary.py
@@ -30,19 +30,18 @@ class BinarySearchTree:
         else:
             return self.search(node.right, target)
 
-    def find_min(self):
-        temp = self.root
+    def find_min(self, node):
+        temp = node
 
         if temp is None:
-            print("ERROR: Binary Tree is empty")
-            return False
+            raise ValueError("Cannot find min of an empty subtree")
 
         while temp.left is not None:
             temp = temp.left
         return temp.data
 
-    def find_max(self):
-        temp = self.root
+    def find_max(self, node):
+        temp = node
 
         if temp is None:
             print("ERROR: Binary Tree is empty")
@@ -101,9 +100,9 @@ class BinarySearchTree:
         if temp is None:
             return
 
-        self.pre_order(temp.left)
+        self.in_order(temp.left)
         print(temp.data, end="->")
-        self.pre_order(temp.right)
+        self.in_order(temp.right)
 
     def post_order(self, node):
         """
@@ -115,9 +114,51 @@ class BinarySearchTree:
         if temp is None:
             return
 
-        self.pre_order(temp.left)
-        self.pre_order(temp.right)
+        self.post_order(temp.left)
+        self.post_order(temp.right)
         print(temp.data, end="->")
+
+    def delete(self, value):
+        self.root = self._delete_node(self.root, value)
+
+    def _delete_node(self, node, value):
+        if node is None:
+            return node
+        elif value < node.data:
+            node.left = self._delete_node(node.left, value)
+        elif value > node.data:
+            node.right = self._delete_node(node.right, value)
+        else:
+            if node.left is None and node.right is None:
+                node = None
+            elif node.left is None:
+                node = node.right
+            elif node.right is None:
+                node = node.left
+            else:
+                temp = self.find_min(node.right)
+                node.data = temp
+                node.right = self._delete_node(node.right, temp)
+
+        return node
+
+    def isBST(self) -> bool:
+        node = self.root
+        return self.is_binary_search_tree(node, float("-inf"), float("inf"))
+
+    def is_binary_search_tree(self, node, min_val, max_val):
+        if node is None:
+            return True
+
+        if (
+            node.data > min_val
+            and node.data < max_val
+            and self.is_binary_search_tree(node.left, min_val, node.data)
+            and self.is_binary_search_tree(node.right, node.data, max_val)
+        ):
+            return True
+        else:
+            return False
 
 
 if __name__ == "__main__":
@@ -127,8 +168,8 @@ if __name__ == "__main__":
         bst.add(val)
 
     print(bst.search(bst.root, 9))
-    print(bst.find_min())
-    print(bst.find_max())
+    print(bst.find_min(bst.root))
+    print(bst.find_max(bst.root))
     print(bst.find_height(bst.root))
     bst.level_order()
     print()
@@ -137,3 +178,8 @@ if __name__ == "__main__":
     bst.in_order(bst.root)
     print()
     bst.post_order(bst.root)
+    print()
+    bst.delete(5)
+    bst.level_order()
+    print()
+    print(bst.isBST())
