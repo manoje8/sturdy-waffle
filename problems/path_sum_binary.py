1b5e806d1229b3eb11112a43f1a40072acdaf499
manoje8 <manodeepan2001@gmail.com>
2026-08-08 23:45:04 +0530

    added bst path sum

---
+++ problems/path_sum_binary.py
@@ -0,0 +1,8 @@
+def has_path_sum(root, target):
+    if not root:
+        return False
+    if not root.left and not root.right:
+        return target == root.val
+    return has_path_sum(root.left, target - root.val) or has_path_sum(
+        root.right, target - root.val
+    )
